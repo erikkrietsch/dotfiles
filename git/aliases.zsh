6f744e3f3ab88d7fc73a97e37ddc425dd3308f74
Erik Krietsch <=>
2022-12-14 09:03:25 -0600

    Add some Git conveniences

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -10,6 +10,7 @@ fi
 alias glog="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --all"
 alias gp='git push origin HEAD'
 alias gd='git diff'
+alias gdc='git diff --cached'
 alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
@@ -19,3 +20,4 @@ alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
 alias gpp='git push origin +$(git rev-parse --abbrev-ref HEAD)'
 alias gdoff='git reset --soft HEAD~1'
+alias gap='git add . --patch'
