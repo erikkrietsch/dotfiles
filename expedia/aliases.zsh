70aef2dcc35abf87fcc1cfdac0b0359e5c874f8d
Erik Krietsch <=>
2024-03-11 11:28:58 -0500

    Add git details, delete derived data

--- expedia/aliases.zsh
+++ expedia/aliases.zsh
@@ -1,2 +1,3 @@
 alias dep-bex='xcodebuild -workspace "ExpediaBookings.xcworkspace" -scheme "ExpediaBookings" -resolvePackageDependencies -usePackageSupportBuiltinSCM'
 alias dep-sui='xcodebuild -workspace "EGSharedUIComponents.xcworkspace" -scheme "Explorer" -resolvePackageDependencies -usePackageSupportBuiltinSCM'
+alias ddd='rm -rf ~/Library/Developer/Xcode/DerivedData'
